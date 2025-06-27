0d65f7e5d5db71efd441dd31941f773548e1a518
kesinilagi <kesinisaja00@gmail.com>
2025-06-27 11:37:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // sw.js - Service Worker Minimalis
 
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Install');
+  console.log('Service Worker: Menginstall...');
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activate');
+  console.log('Service Worker: Mengaktifkan...');
 });
 
+// Listener fetch sederhana ini sudah cukup untuk memenuhi kriteria PWA
 self.addEventListener('fetch', (event) => {
-  // Tidak melakukan caching, hanya memenuhi syarat PWA
   event.respondWith(fetch(event.request));
 });
